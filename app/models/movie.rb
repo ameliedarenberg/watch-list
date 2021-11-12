4962066f7df2f20d844942b23d3a3a1bc95644e5
ameliedarenberg <amelie.arenberg@icloud.com>
2021-11-12 12:03:31 +0100

    done

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,5 @@
 class Movie < ApplicationRecord
-  validates :title, presence: true, uniqueness: true
-  has_many :bookmarks, dependent: :destroy
+  validates :title, presence: true, uniqueness: true, allow_blank: false #?
+  has_many :bookmarks
+  validates :overview, presence: true
 end
